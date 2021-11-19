4f6a0460cccacb11ba65dae6b2a39ea6270af16c
Nitay Bachrach <nitay.bachrach@gmail.com>
2021-11-19 12:29:03 +0200

    v0.1.2

---
+++ sindri/consts.py
@@ -0,0 +1,3 @@
+import os
+
+SINDRI_CONFIG = os.path.expanduser(os.getenv("SINDRI_PATH", os.path.join("~",".config","sindri")))
--- sindri/supplier_amass.py
+++ sindri/supplier_amass.py
@@ -1,10 +1,17 @@
 import subprocess
 import shlex
+import os
 
 from .exceptions import SindriSupplierNotFound, SindriSupplierFailedError
+from .consts import SINDRI_CONFIG
 
 def get_subdomains(domain):
-    amass_command = ["amass", "enum", "-nocolor", "-passive", "-d", domain]
+    config_path = os.getenv("AMASS_CONFIG", os.path.join(SINDRI_CONFIG, "amass.ini"))
+    if os.path.exists(config_path):
+        amass_command = ["amass", "enum", "-config", config_path , "-nocolor", "-passive", "-d", domain]
+    else:
+        amass_command = ["amass", "enum", "-nocolor", "-passive", "-d", domain]
+
     print(f"Running amass: {shlex.join(amass_command)}")
     try:
         amass_process = subprocess.Popen(amass_command, stdout=subprocess.PIPE)
